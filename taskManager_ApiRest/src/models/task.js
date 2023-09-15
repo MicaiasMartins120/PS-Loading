515ceab05f6ed094dad2e5d1b2228446dacd1143
MicaiasMartins120 <106540059+MicaiasMartins120@users.noreply.github.com>
2023-09-15 20:49:24 -0300

    Add files via upload

--- taskManager_ApiRest/src/models/task.js
+++ taskManager_ApiRest/src/models/task.js
@@ -1,1 +1,28 @@
+const mongoose = require("mongoose");
 
+const { Schema } = mongoose;
+
+const taskSchema = new Schema({
+	title: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	status: {
+		type: String,
+		required: true,
+		enum: ["Pendente", "Em andamento", "Concluida"],
+	},
+}, { timestamps: true});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
